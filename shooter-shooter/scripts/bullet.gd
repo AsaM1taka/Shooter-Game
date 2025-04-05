5bd2813256e987a2137d5ec5a49a59ac4880e7c8
amy <C24399391@myTUDublin.ie>
2025-04-05 15:14:45 +0100

    Bullet sprites are randomized

--- shooter-shooter/scripts/bullet.gd
+++ shooter-shooter/scripts/bullet.gd
@@ -1,12 +1,21 @@
 extends Area2D
 class_name Bullet
 
+@export var bullet_sprites: Array = [ preload("res://Assets/note 1.png"),
+									  preload("res://Assets/note2.png"),
+									  preload("res://Assets/note3.png")]
+
 var direction: Vector2
 
 const SPEED = 400
 
+var sprite: Sprite2D
+
 func _ready():
 	direction = direction.normalized()
+	sprite = $Sprite2D
+	var random_index = randi() % bullet_sprites.size()
+	sprite.texture = bullet_sprites[random_index]
 
 func _physics_process(delta: float) -> void:
 	global_position += direction * SPEED * delta
